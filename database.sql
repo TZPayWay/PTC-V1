53ae18d51d567d0a7b027074cafd2597a3ed4350
zihadurrahmandev <zihadurrahman.dev@gmail.com>
2026-08-17 18:25:12 +0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO `gateways` (`code`, `name`, `alias`, `image`, `status`, `gateway_parameters`, `supported_currencies`, `crypto`, `description`, `created_at`, `updated_at`) 
 VALUES 
-(133, 'TZPAYWAY', 'TZPAYWAY', 'tzpayway.png', 1, '{"api_key":{"title":"API Key","global":true,"value":""},"api_url":{"title":"API Base URL","global":true,"value":"https://tzpayway.com"}}', '{"BDT":"BDT","USD":"USD"}', 0, 'Pay securely via bKash, Nagad, Rocket, Upay, Bank Transfer, and Crypto through TZPayWay.', NOW(), NOW())
+(133, 'TZPAYWAY', 'TZPAYWAY', 'tzpayway.png', 1, '{"api_key":{"title":"API Key","global":true,"value":""},"secret_key":{"title":"Secret Key","global":true,"value":""},"api_url":{"title":"API Base URL","global":true,"value":"https://tzpayway.com"}}', '{"BDT":"BDT","USD":"USD"}', 0, 'Pay securely via bKash, Nagad, Rocket, Upay, Bank Transfer, and Crypto through TZPayWay.', NOW(), NOW())
 ON DUPLICATE KEY UPDATE 
 `name` = VALUES(`name`),
 `alias` = VALUES(`alias`),
